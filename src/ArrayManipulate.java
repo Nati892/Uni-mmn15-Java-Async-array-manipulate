758f2757acb5d53b299c838777480dd3b3beb808
Nati892 <93869199+Nati892@users.noreply.github.com>
2022-05-19 14:23:23 +0300

    not all working

--- src/ArrayManipulate.java
+++ src/ArrayManipulate.java
@@ -1,2 +1,104 @@
-public class some {
+import java.util.Scanner;
+
+public class ArrayManipulate {
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        runProgram();
+    }
+
+
+    private static void runProgram() {
+        System.out.println("Hi, Please enter the threads array size");
+        int arrSize = inputPositiveNum(2);
+        System.out.println("Please enter the number of rounds to do");
+        int roundsAmount = inputPositiveNum(1);
+
+        IndexThread[] threadsArr = new IndexThread[arrSize];
+
+        initArray(threadsArr);
+        printArray(threadsArr);
+
+        for (int i = 0; i < threadsArr.length; i++) {
+            threadsArr[i].start();
+        }
+
+        for (int i = 0; i < threadsArr.length; i++) {//wait for thread to end
+            while (threadsArr[i].getRunning()) {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        printArray(threadsArr);
+
+        for (int i = 1; i < roundsAmount; i++) {
+            System.out.println("************** round " + i);
+            mIterate(threadsArr);
+        }
+        System.out.println("**************** Done ***************");
+    }
+
+
+    private static void initArray(IndexThread[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new IndexThread(i, arr);
+        }
+
+    }
+
+    private static void mIterate(IndexThread[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].reset();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].setRunning(true);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i].getRunning()) {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        printArray(arr);
+    }
+
+    //get an int from the user
+    private static int inputPositiveNum(int min) {
+        int res = 0;
+        boolean good_input = false;
+        while (!good_input) {
+            try {
+                System.out.println("Please enter an integer of minimum " + min);
+                res = Integer.parseInt(scanner.next());
+                if (res >= min)
+                    good_input = true;
+                else
+                    System.out.println("The number must be a minimum of " + min);
+            } catch (Exception e) {
+                System.out.println("Please re-enter integer");
+            }
+        }
+        return res;
+    }
+
+
+    private static void printArray(IndexThread[] arr) {
+        if (arr.length > 0)
+            System.out.print(" Printed array [" + arr[0].getValue());
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print("," + arr[i].getValue());
+            arr[i].gotValue();
+        }
+        System.out.println("]");
+    }
+
 }
--- src/IndexThread.java
+++ src/IndexThread.java
@@ -1,2 +1,127 @@
-public class IndexThread {
+public class IndexThread extends Thread {
+    private boolean running;
+    private boolean EndThread;
+    private boolean gettingChecked;
+    public int index;//the threads index TODO private
+    private IndexThread[] arr;//the array
+    private int value;
+    private boolean LeftCheckedMe;
+    private boolean RightCheckedMe;
+
+    public IndexThread(int index, IndexThread[] arr) {
+        this.running = true;
+        this.EndThread = false;
+        this.index = index;
+        this.arr = arr;
+        this.value = (int) (Math.random() * 100) + 1;
+        this.LeftCheckedMe = false;
+        this.RightCheckedMe = false;
+        this.gettingChecked = false;
+    }
+
+
+    @Override
+    public void run() {
+        super.run();
+        while (!EndThread) {
+            if (running) {
+                System.out.println("thread " + this.index + " started");
+                boolean checkedOthers = false;
+                int leftIndex = ((this.index - 1) % arr.length + arr.length) % arr.length;
+                int rightIndex = ((this.index + 1) % arr.length + arr.length) % arr.length;
+
+                int leftVal;
+                int rightVal;
+
+                if (index % 2 == 1) {
+                    System.out.println("thread  " + this.index + "checking " + leftIndex);
+                    leftVal = arr[leftIndex].getValue(this.index);
+                    arr[leftIndex].gotValue();
+
+                    System.out.println("thread  " + this.index + "checking " + rightIndex);
+                    rightVal = arr[rightIndex].getValue(this.index);
+                    arr[rightIndex].gotValue();
+                } else {
+
+
+                    System.out.println("thread  " + this.index + "checking " + rightIndex);
+                    rightVal = arr[rightIndex].getValue(this.index);
+                    arr[rightIndex].gotValue();
+
+                    System.out.println("thread  " + this.index + "checking " + leftIndex);
+                    leftVal = arr[leftIndex].getValue(this.index);
+                    arr[leftIndex].gotValue();
+                }
+
+                while (!checkedOthers) {
+                    if (LeftCheckedMe && RightCheckedMe) {
+
+                        if (leftVal > this.value && rightVal > this.value) {
+                            this.value++;
+                        } else if (leftVal < this.value && rightVal < this.value) {
+                            this.value--;
+                        }
+
+                        checkedOthers = true;
+                    }
+                }
+
+                System.out.println("thread " + this.index + " ended");
+            }
+            running = false;
+        }
+
+    }
+
+
+    public synchronized int getValue() {
+        while (gettingChecked) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        gettingChecked = true;
+        return this.value;
+    }
+
+    public synchronized int getValue(int checkerIndex) {
+
+
+        if (checkerIndex == (((this.index + 1) % arr.length + arr.length) % arr.length)) {
+            this.RightCheckedMe = true;
+        }
+
+        if (checkerIndex == (((this.index - 1) % arr.length + arr.length) % arr.length)) {
+            this.LeftCheckedMe = true;
+        }
+        return this.value;
+    }
+
+
+    public synchronized void gotValue() {
+        this.gettingChecked = false;
+        notifyAll();
+    }
+
+    public void endProcess() {
+        this.EndThread = true;
+    }
+
+    public  boolean getRunning() {
+        return this.running;
+    }
+
+
+    public synchronized void reset(){
+        this.LeftCheckedMe = false;
+        this.RightCheckedMe = false;
+        this.gettingChecked = false;
+        notifyAll();
+    }
+    public synchronized void setRunning(boolean running) {
+        System.out.println("restarting "+index);
+        this.running = running;
+    }
 }
